fe0e39ec17d61b1cc144c47eb04c266a8e9c935a
ilajordan <ilan.souza@estudante.ifms.edu.br>
2026-03-06 12:27:43 -0400

    ilan....

---
+++ backend/src/todos/repository/ts create-todo.repository.ts
@@ -0,0 +1,14 @@
+import { PrismaService } from "src/shared/databases/prisma.database";
+import { CreateTodoDto } from "../dto/create-todo.dto";
+import { Injectable } from "@nestjs/common";
+
+
+
+@Injectable()
+export class createTodoRepository{
+    constructor(private readonly prisma: PrismaService) {}
+
+    async execute(data: CreateTodoDto) {
+        return await this.prisma.todo.create({ data });
+    }
+}
